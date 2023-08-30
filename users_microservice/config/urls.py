a69c69ca79ad7f25ad0d5061f2d056f230483ccd
Anton_Dubovitskii <Gtoll@yandex.ru>
2023-08-31 00:19:35 +0400

    Добавил документацию для API (Swagger)

--- users_microservice/config/urls.py
+++ users_microservice/config/urls.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import include, path, re_path
+from rest_framework import routers, permissions
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from users import views
 from users.views import RegisterUserAPIView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="users_microservice",
+        default_version='0.1',
+        description="Documentation to out project",
+        contact=openapi.Contact(email="gtoll@yandex.ru"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -17,4 +31,11 @@ urlpatterns = [
     path('api/jwt-token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls)),
     path('api/register/', RegisterUserAPIView.as_view()),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
