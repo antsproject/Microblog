5bb55e4f509e983d224e3e6d5ab9aacf7fc0382e
Anton_Dubovitskii <Gtoll@yandex.ru>
2023-09-05 19:27:38 +0400

    Вернул дефолтное значение is_active на True, пока нет системы подтверждения по почте

--- users_microservice/app/users/models.py
+++ users_microservice/app/users/models.py
@@ -38,7 +38,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
 
     groups = models.ManyToManyField(
