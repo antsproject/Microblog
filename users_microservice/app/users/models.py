c29654d17faea9c61692d3d2afa3f0391b3c2d00
Anton_Dubovitskii <Gtoll@yandex.ru>
2023-09-05 13:44:55 +0400

    Добавил поле is_active в CustomUser

--- users_microservice/app/users/models.py
+++ users_microservice/app/users/models.py
@@ -37,6 +37,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     groups = models.ManyToManyField(
