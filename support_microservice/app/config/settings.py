c860f63d56d92286a290483fab47358665fba5ed
maksik <mzagrebam@gmail.com>
2023-09-10 21:53:52 +0300

    add pagination for messages and fix unique emails

--- support_microservice/app/config/settings.py
+++ support_microservice/app/config/settings.py
@@ -147,3 +147,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
