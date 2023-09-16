f383a5c79b48f825c143ea2264b1e219e0c92023
Alex Glagolev <gofavalon@gmail.com>
2023-09-16 15:52:37 +0300

    Tags+Safe_del
    Themes on post, with only Staff permissions; safe delete for posts, flag is_deleted

--- posts_microservice/app/posts/models.py
+++ posts_microservice/app/posts/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from uuid import uuid4
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
 class PostModel(models.Model):
     id = models.UUIDField(default=uuid4, primary_key=True, editable=False)
     username = models.CharField(max_length=64, unique=False)
@@ -9,6 +16,8 @@ class PostModel(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
 
     class Meta:
         db_table = "posts"
