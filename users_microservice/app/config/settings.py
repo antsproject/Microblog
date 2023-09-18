6ea2271024c57307ce551789684170090aab8e3b
Anton_Dubovitskii <Gtoll@yandex.ru>
2023-09-18 15:42:51 +0400

    Обновление политики CORS у users, временно разрешены все источники

--- users_microservice/app/config/settings.py
+++ users_microservice/app/config/settings.py
@@ -63,10 +63,12 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000'
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:3000',
+#     'http://localhost:3000'
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'config.urls'
 
