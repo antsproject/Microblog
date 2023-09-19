4f859671f865c9607c66f54b3c7521f40353faa1
unknown <neatek@icloud.com>
2023-09-19 19:11:47 +0300

    Some fixes for logout menu in miniprofile

--- frontend/app/src/components/ProfileMini/LogoutModal.jsx
+++ frontend/app/src/components/ProfileMini/LogoutModal.jsx
@@ -2,26 +2,36 @@ import React from "react";
 import User from '../../images/user-03.svg';
 import Shield from '../../images/shield-02.svg';
 import Logout from '../../images/log-out-01.svg';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setToken } from '../../features/tokenSlice.js';
 
-const LogoutModal = ({ onClose }) => {
+const LogoutModal = ({ onCloseTrigger }) => {
+  const dispatch = useDispatch();
+
   const handleLogout = () => {
     // Добавьте здесь логику для выхода пользователя
     // Например, вызов функции для разлогинивания пользователя
     // Или перенаправление на страницу выхода
 
     // Закройте модальное окно после выполнения операции
-    onClose();
+    localStorage.removeItem('accessToken');
+    dispatch(setToken(false));
+    onCloseTrigger();
   };
 
   return (
     <div>
-      <a href="#" className="profile-mini-text-elements" onClick={handleLogout}>
-      <img src={User}/>Мой Профиль</a >
-      <a href="#" className="profile-mini-text-elements" onClick={handleLogout}>
-      <img src={Shield}/>Настройки</a >
-      <a href="#" className="profile-mini-text-elements" onClick={handleLogout}>
-      <img src={Logout}/>Выйти</a >
-      <a href="#" className="profile-mini-text-elements" onClick={onClose}>X</a >
+      <Link to="/me" className="profile-mini-text-elements" onClick={handleLogout}>
+        <img src={User} />Мой профиль
+      </Link>
+      <Link to="/user" className="profile-mini-text-elements" onClick={handleLogout}>
+        <img src={Shield} />Настройки
+      </Link>
+      <Link to="#" className="profile-mini-text-elements" onClick={handleLogout}>
+        <img src={Logout} />Выйти
+      </Link>
+      {/* <a href="#" className="profile-mini-text-elements" onClick={onCloseTrigger}>X</a > */}
     </div>
   );
 };
--- frontend/app/src/components/ProfileMini/ProfileMini.jsx
+++ frontend/app/src/components/ProfileMini/ProfileMini.jsx
@@ -1,43 +1,46 @@
 import React, { useState } from 'react';
 import profileMini from '../../images/miniprofile.jpg';
+import LogoutModal from './LogoutModal';
 import './ProfileMini.css';
-import LogoutModal from './LogoutModal'; // Импортируйте компонент модального окна
 
 const ProfileMini = () => {
   const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
 
-  const openLogoutModal = () => {
-    setIsLogoutModalOpen(true);
+  // const openLogoutModal = () => {
+  //   setIsLogoutModalOpen(true);
+  //   console.debug('openLogoutModal', isLogoutModalOpen);
+  // };
+
+  // const closeLogoutModal = () => {
+  //   setIsLogoutModalOpen(isLogoutModalOpen ? false : true);
+  //   console.debug('closeLogoutModal', isLogoutModalOpen);
+  // };
+
+  const toggleLogoutModal = () => {
+    setIsLogoutModalOpen(isLogoutModalOpen ? false : true);
+    // console.debug('closeLogoutModal', isLogoutModalOpen);
   };
 
-  const closeLogoutModal = () => {
-    setIsLogoutModalOpen(false);
-  };
-
-  const handleBodyClick = () => {
-    closeLogoutModal();
-  };
+  // const handleBodyClick = () => {
+  //   toggleLogoutModal();
+  // };
 
   return (
-    <>
+    <div className='profile-mini' onClick={toggleLogoutModal}>
       <p className="profile-mini__name">Владимир Желнов</p>
       <div className="profile-mini__img-container">
         <img
           className="profile-mini__img"
           src={profileMini}
           alt="profile"
-          onClick={openLogoutModal} // Добавьте обработчик клика
         />
         {isLogoutModalOpen && (
-          <div
-            className="logout-modal"
-            // Предотвращаем закрытие при клике внутри модального окна
-          >
-            <LogoutModal onClose={closeLogoutModal} onClick={(e) => e.stopPropagation()} />
+          <div className="logout-modal">
+            <LogoutModal onCloseTrigger={toggleLogoutModal} onClick={(e) => e.stopPropagation()} />
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
