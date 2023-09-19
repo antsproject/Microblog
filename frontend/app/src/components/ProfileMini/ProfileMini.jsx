dbc5b4ddc60776f2d73a8504eef2661b6a4b37df
maksik <mzagrebam@gmail.com>
2023-09-19 18:41:04 +0300

    add mini-window

--- frontend/app/src/components/ProfileMini/ProfileMini.jsx
+++ frontend/app/src/components/ProfileMini/ProfileMini.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import profileMini from '../../images/miniprofile.jpg';
 import './ProfileMini.css';
+import LogoutModal from './LogoutModal'; // Импортируйте компонент модального окна
+
 const ProfileMini = () => {
+  const [isLogoutModalOpen, setIsLogoutModalOpen] = useState(false);
+
+  const openLogoutModal = () => {
+    setIsLogoutModalOpen(true);
+  };
+
+  const closeLogoutModal = () => {
+    setIsLogoutModalOpen(false);
+  };
+
+  const handleBodyClick = () => {
+    closeLogoutModal();
+  };
+
   return (
     <>
-      {/* <div className="profile-mini"> */}
       <p className="profile-mini__name">Владимир Желнов</p>
-      <img className="profile-mini__img" src={profileMini} alt="profile" />
-      {/* </div> */}
+      <div className="profile-mini__img-container">
+        <img
+          className="profile-mini__img"
+          src={profileMini}
+          alt="profile"
+          onClick={openLogoutModal} // Добавьте обработчик клика
+        />
+        {isLogoutModalOpen && (
+          <div
+            className="logout-modal"
+            // Предотвращаем закрытие при клике внутри модального окна
+          >
+            <LogoutModal onClose={closeLogoutModal} onClick={(e) => e.stopPropagation()} />
+          </div>
+        )}
+      </div>
     </>
   );
 };
