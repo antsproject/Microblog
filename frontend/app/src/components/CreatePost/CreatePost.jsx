403e35642224802849fe9e90d9eaeb8fed76f766
Tishev <tishev5667@bk.ru>
2023-09-21 22:01:24 +0300

    почти доделал страницу создания поста

--- frontend/app/src/components/CreatePost/CreatePost.jsx
+++ frontend/app/src/components/CreatePost/CreatePost.jsx
@@ -4,10 +4,19 @@ import { BlockNoteEditor } from '@blocknote/core';
 import { BlockNoteView, useBlockNote } from '@blocknote/react';
 import '@blocknote/core/style.css';
 import skrepka from '../../images/skrepka.svg';
+import axios from 'axios';
 
 const CreatePost = () => {
-  const editor = useBlockNote({});
+  const editor = useBlockNote({
+    // тут отслеживаем каждое изменение в редакторе
+    onEditorContentChange: (editor) => setBlocks(editor.topLevelBlocks),
+  });
+  // сюда сохраняем/записываем состояние файла(изображения)
   const [selectedFile, setSelectedFile] = useState(null);
+  // сюда сохраняем/записываем состояние редактора, которое на 12 строке
+  const [blocks, setBlocks] = useState(null);
+  // сюда сохраняем/записываем состояние редактора и файла
+  const [allData, setAllData] = useState([]);
 
   // Обработчик изменения выбранного файла
   const handleFileChange = (e) => {
@@ -18,16 +27,28 @@ const CreatePost = () => {
   // Обработчик отправки файла (здесь вы можете добавить логику для отправки файла на сервер)
   const handleFileUpload = () => {
     if (selectedFile) {
-      // Здесь может быть ваш код для отправки файла на сервер
       console.log('Отправка файла:', selectedFile);
+      setAllData(allData.push((item) => item));
     } else {
       alert('Выберите файл для загрузки.');
     }
   };
+  // Обработчик отправки редактора текста и файла
+  const handleSubmitOnServer = () => {
+    try {
+      // тут нужно добавить запрос на сервер
+      const response = axios.get('', allData);
+      console.log(response.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
-    <div className="create-post whitebox">
+    <form onSubmit={handleSubmitOnServer} className="create-post whitebox">
       <BlockNoteView editor={editor} />
+      {/* эта строка выводит структуру объекта Blocks */}
+      <pre> {JSON.stringify(blocks, null, 2)}</pre>
       <div className="create-post__submit">
         <label className="create-post__input">
           <img src={skrepka} alt="skrepka" />
@@ -39,7 +60,7 @@ const CreatePost = () => {
           Опубликовать
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
