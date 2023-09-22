5bcd9bf910ac7cf8525aef628246dc3468e95e55
Alex Glagolev <gofavalon@gmail.com>
2023-09-22 13:41:31 +0300

    CORS
    CORS_ALLOW_ALL_ORIGINS = True

--- posts_microservice/app/config/settings.py
+++ posts_microservice/app/config/settings.py
@@ -40,7 +40,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,10 +50,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000'
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:3000',
+#     'http://localhost:3000'
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 
