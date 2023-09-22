5dcedf0c14e151c5bf92b9008cf9efcf1aebc658
Tishev <tishev5667@bk.ru>
2023-09-22 20:06:54 +0300

    Верстка создания поста

--- frontend/app/src/components/CreatePost/CreatePost.jsx
+++ frontend/app/src/components/CreatePost/CreatePost.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './CreatePost.css';
-import { BlockNoteEditor } from '@blocknote/core';
 import { BlockNoteView, useBlockNote } from '@blocknote/react';
 import '@blocknote/core/style.css';
 import skrepka from '../../images/skrepka.svg';
@@ -16,28 +15,32 @@ const CreatePost = () => {
   // сюда сохраняем/записываем состояние редактора, которое на 12 строке
   const [blocks, setBlocks] = useState(null);
   // сюда сохраняем/записываем состояние редактора и файла
-  const [allData, setAllData] = useState([]);
+  // const [allData, setAllData] = useState([]);
 
   // Обработчик изменения выбранного файла
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setSelectedFile(file);
   };
-
   // Обработчик отправки файла (здесь вы можете добавить логику для отправки файла на сервер)
-  const handleFileUpload = () => {
-    if (selectedFile) {
-      console.log('Отправка файла:', selectedFile);
-      setAllData(allData.push((item) => item));
-    } else {
-      alert('Выберите файл для загрузки.');
-    }
-  };
+  // const handleFileUpload = () => {
+  //   if (selectedFile) {
+  //     console.log('Отправка файла:', selectedFile);
+  //     setAllData([...allData]);
+  //   } else {
+  //     alert('Выберите файл для загрузки.');
+  //   }
+  // };
   // Обработчик отправки редактора текста и файла
-  const handleSubmitOnServer = () => {
+  const handleSubmitOnServer = async (e) => {
+    e.preventDefault();
     try {
-      // тут нужно добавить запрос на сервер
-      const response = axios.get('', allData);
+      const formData = new FormData();
+      formData.append('text', JSON.stringify(blocks)); // Отправляем структуру редактора текста
+      formData.append('file', selectedFile); // Отправляем файл
+
+      const response = await axios.post('http://localhost:8082/api/posts/', formData);
+      console.log(formData);
       console.log(response.data);
     } catch (e) {
       console.error(e);
@@ -48,7 +51,7 @@ const CreatePost = () => {
     <form onSubmit={handleSubmitOnServer} className="create-post whitebox">
       <BlockNoteView editor={editor} />
       {/* эта строка выводит структуру объекта Blocks */}
-      <pre> {JSON.stringify(blocks, null, 2)}</pre>
+      {/* <pre> {JSON.stringify(blocks, null, 2)}</pre> */}
       <div className="create-post__submit">
         <label className="create-post__input">
           <img src={skrepka} alt="skrepka" />
@@ -56,9 +59,7 @@ const CreatePost = () => {
           Изображение
         </label>
 
-        <button onClick={handleFileUpload} className="btn-red">
-          Опубликовать
-        </button>
+        <button className="btn-red">Опубликовать</button>
       </div>
     </form>
   );
