6f54e56e1456f3c241eaf1154ce21a461f485019
Alex Glagolev <gofavalon@gmail.com>
2023-09-28 00:59:03 +0300

    New Docs + Models
    - New Image fields in Posts and Categories: Resized
    - New Docs
    - New Likes endpoint
    - New Random Posts

--- posts_microservice/app/posts/management/commands/create_test_post.py
+++ posts_microservice/app/posts/management/commands/create_test_post.py
@@ -1,34 +1,56 @@
 from django.core.management.base import BaseCommand
 from posts.models import PostModel, CategoryModel
-import random
+from mimesis import Generic
+from mimesis.locales import Locale
+from mimesis import Text
+import json
+
+TEST_POST_COUNT = 10
+
+generic = Generic(locale=Locale.RU)
+categories = CategoryModel.objects.all().order_by('id')
 
 
 class Command(BaseCommand):
-    help = 'Create 150 test posts'
+    help = f'Create {TEST_POST_COUNT} test posts'
 
     def handle(self, *args, **kwargs):
-        category = CategoryModel.objects.all()
-        num_test_posts = 150
+        for _ in range(1, TEST_POST_COUNT + 1):
+            sentences = generic.random.randint(3, 10)
+            fake_title = Text().title()
+            fake_text = Text().text(sentences)
 
-        for i in range(num_test_posts):
-            example_posts = {
-                "user_id": random.randint(0, 100000),
-                "title": f"Example Post {i}",
-                "content": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium"
-                           "voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati"
-                           "cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi,"
-                           "id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio."
-                           "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id"
-                           "quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus."
-                           "Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet "
-                           "ut et voluptates repudiandae sint et molestiae non recusandae. "
-                           "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores"
-                           "alias consequatur aut perferendis doloribus asperiores repellat",
-                "category": random.choice(category),
+            fake_content = {
+                "id": generic.random.randint(1, 100),
+                "type": "paragraph",
+                "props": {
+                    "textColor": "black",
+                    "backgroundColor": "white",
+                    "textAlignment": generic.random.choice(["left", "center", "right"]),
+                },
+                "content": [
+                    {
+                        "type": "text",
+                        "text": fake_text,
+                        "styles": {
+                            "fontFamily": generic.text.word(),
+                            "fontSize": generic.random.randint(12, 24),
+                        },
+                    }
+                ],
+                "children": [],
             }
 
-            PostModel.objects.create(**example_posts)
+            fake_json_content = json.dumps(fake_content, indent=4)
 
-        self.stdout.write(
-            self.style.SUCCESS('150 Example posts created successfully [TEST POSTS SUCCESS].')
-        )
+            example_post = {
+                "user_id": generic.random.randint(1, 7),
+                "category": generic.random.choice(categories),
+                "title": fake_title,
+                "content": fake_json_content
+            }
+
+            PostModel.objects.create(**example_post)
+
+            self.stdout.write(
+                self.style.SUCCESS(f'{_}/{TEST_POST_COUNT} Example post created successfully [SUCCESS].'))
