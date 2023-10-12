2fb5cc4fa606d6bb9e693dc01276dcf090410ddd
Tishev <tishev5667@bk.ru>
2023-10-12 17:32:58 +0300

    сделал прилипание левой стороны верстки

--- frontend/blogish/components/Layout.jsx
+++ frontend/blogish/components/Layout.jsx
@@ -1,50 +1,81 @@
 import { useState } from 'react';
 import Header from './Header.jsx';
-import Link from "next/link";
-import Image from "next/image";
-import { useRouter } from "next/router";
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export default function Layout({ centerHeader, children }) {
-	const [active, setActive] = useState(false);
-	const router = useRouter();
+    const [active, setActive] = useState(false);
+    const router = useRouter();
 
-	const handleClosePopup = () => {
-		setActive(!active);
-	};
+    const handleClosePopup = () => {
+        setActive(!active);
+    };
 
-	return (
-		<>
-			<Header active={active} handleClosePopup={handleClosePopup} />
-			<div className="wrapper">
-				<div className="container-wrapper">
-					<div className="wrapper-left">
-						<Link href="/popular" className={`wrapper-left__link ${router.pathname === '/popular' ? 'hovered' : ''}`}>
-							<Image src="/images/rocket-02.svg" width={24} height={24} alt='' /> Популярное
-						</Link>
-						<Link href="/new" className={`wrapper-left__link ${router.pathname === '/new' ? 'hovered' : ''}`}>
-							<Image src="/images/clock.svg" width={24} height={24} alt='' /> Свежее
-						</Link>
-						<Link href="/subscriptions" className={`wrapper-left__link ${router.pathname === '/subscriptions' ? 'hovered' : ''}`}>
-							<Image src="/images/zap-fast.svg" width={24} height={24} alt='' /> Подписки
-						</Link>
-						<Link href="/favorite" className={`wrapper-left__link ${router.pathname === '/favorite' ? 'hovered' : ''}`}>
-							<Image src="/images/bookmark-check.svg" width={24} height={24} alt='' /> Избранное
-						</Link>
-						<div className="wrapper-left__bottom">
-							<Link href="#">Заказать рекламу</Link>
-							<Link href="#">О проекте</Link>
-							<Link href="#">Вакансии</Link>
-						</div>
-					</div>
-					<div className="wrapper-center">
-						{centerHeader}
-						{children}
-					</div>
-					<div className="wrapper-right">
-						<Image src="/images/adv.svg" width={210} height={800} alt='adv' priority />
-					</div>
-				</div>
-			</div>
-		</>
-	);
-};
+    return (
+        <>
+            <Header active={active} handleClosePopup={handleClosePopup} />
+            <div className="wrapper">
+                <div className="container-wrapper">
+                    <div className="wrapper-left">
+                        <div className="wrapper-sticky">
+                            <Link
+                                href="/popular"
+                                className={`wrapper-left__link ${
+                                    router.pathname === '/popular' ? 'hovered' : ''
+                                }`}
+                            >
+                                <Image src="/images/rocket-02.svg" width={24} height={24} alt="" />{' '}
+                                Популярное
+                            </Link>
+                            <Link
+                                href="/new"
+                                className={`wrapper-left__link ${
+                                    router.pathname === '/new' ? 'hovered' : ''
+                                }`}
+                            >
+                                <Image src="/images/clock.svg" width={24} height={24} alt="" />{' '}
+                                Свежее
+                            </Link>
+                            <Link
+                                href="/subscriptions"
+                                className={`wrapper-left__link ${
+                                    router.pathname === '/subscriptions' ? 'hovered' : ''
+                                }`}
+                            >
+                                <Image src="/images/zap-fast.svg" width={24} height={24} alt="" />{' '}
+                                Подписки
+                            </Link>
+                            <Link
+                                href="/favorite"
+                                className={`wrapper-left__link ${
+                                    router.pathname === '/favorite' ? 'hovered' : ''
+                                }`}
+                            >
+                                <Image
+                                    src="/images/bookmark-check.svg"
+                                    width={24}
+                                    height={24}
+                                    alt=""
+                                />{' '}
+                                Избранное
+                            </Link>
+                            <div className="wrapper-left__bottom">
+                                <Link href="#">Заказать рекламу</Link>
+                                <Link href="#">О проекте</Link>
+                                <Link href="#">Вакансии</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="wrapper-center">
+                        {centerHeader}
+                        {children}
+                    </div>
+                    <div className="wrapper-right">
+                        <Image src="/images/adv.svg" width={210} height={800} alt="adv" priority />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
