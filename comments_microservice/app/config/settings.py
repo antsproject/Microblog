03a638e2895968217491e34e461c8d71e9de724a
Anton_Dubovitskii <Gtoll@yandex.ru>
2023-10-13 23:26:59 +0400

    Возможный фикс для корсов комментов

--- comments_microservice/app/config/settings.py
+++ comments_microservice/app/config/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '*',
+    '0.0.0.0',
+]
 
 
 # Application definition
@@ -51,11 +54,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 
 ]
 
